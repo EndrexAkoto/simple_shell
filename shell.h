2e4b8dc85df96518e02bbdaa4d6612148d0d5a13
EndrexAkoto <martinakoto3@gmail.com>
2023-05-13 05:45:56 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#define SHELL_H
+#ifndef SHELL_H
+
+#include<shell.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<limits.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<string.h>
+
+#endif
