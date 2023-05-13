4e3e3952d0edc2e7e8eed99a76f42ff5323bac40
EndrexAkoto <martinakoto3@gmail.com>
2023-05-13 05:46:35 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+*Main: the entry point for a code
+*@ac: handling arg count
+*@av: handling arg vector
+*
+*Return: 0 success, 1 error
+*/
+int main(int ac, char **av)
+{
+info_t[] = {INFO_INT};
+int inf = 2;
+
+return (EXIT_SUCCESS);
+}
