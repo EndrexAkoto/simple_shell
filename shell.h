e0d6b156f9bcb861abb526db28e2c6167e8ae09d
EndrexAkoto <martinakoto3@gmail.com>
2023-05-15 14:07:38 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -39,5 +39,16 @@ int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
+/*toem_getenv.c*/
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/*toem_atoi.c*/
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
 #endif
 
