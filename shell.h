ecedca1729daa770e8978415e3a09e5376267777
EndrexAkoto <martinakoto3@gmail.com>
2023-05-15 10:50:02 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -13,16 +13,31 @@
 #include<fcntl.h>
 #include<string.h>
 
-*@env: copy list of environ
-*@environ: modified copy environ from LL env
-*env_changed: when environment is changed,turn on
+/*command chaining*/
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+/*system getline()*/
+#define USE_GETLINE 0
+#define STROK 0
+
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX 4096
+
+extern char **environ;
+
+*@env : copy list of environ
+* @environ : modified copy environ from LL env
+* env_changed : when environment is changed, turn on
 
 /*toem_environ.c*/
-char *_getenv(info_t*, const char*);
-int *_myenv(info_t*);
-int _mysetenv(info_t*);
-int _myunsetenv(info_t*);
-int populate_env_list(info_t*);
+char *_getenv(info_t *, const char*);
+int *_myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
 
 #endif
 
